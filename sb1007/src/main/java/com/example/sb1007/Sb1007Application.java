8f8ccce566a66dfe2be6962a0676953d347f4288
ChangHyun <19941128o@gmail.com>
2024-10-07 18:35:58 +0900

    commit

---
+++ sb1007/src/main/java/com/example/sb1007/Sb1007Application.java
@@ -0,0 +1,13 @@
+package com.example.sb1007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1007Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb1007Application.class, args);
+    }
+
+}
