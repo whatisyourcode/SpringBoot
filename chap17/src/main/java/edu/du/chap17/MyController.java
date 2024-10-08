9df77a1b054ec55a8b967a748a1c1fa7dc9f4076
ChangHyun <19941128o@gmail.com>
2024-10-08 19:47:06 +0900

    springboot 20241008

---
+++ chap17/src/main/java/edu/du/chap17/MyController.java
@@ -0,0 +1,64 @@
+package edu.du.chap17;
+
+import edu.du.chap17.model.ArticleListModel;
+import edu.du.chap17.service.ArticleNotFoundException;
+import edu.du.chap17.service.ListArticleService;
+import edu.du.chap17.service.ReadArticleService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @Autowired
+    ListArticleService listSerivce;
+    @Autowired
+    ReadArticleService readSerivce;
+
+    @GetMapping("/list")
+    public String list(Model model, HttpServletRequest request) {
+        String pageNumberString = request.getParameter("p");
+        int pageNumber = 1;
+        if (pageNumberString != null && pageNumberString.length() > 0) {
+            pageNumber = Integer.parseInt(pageNumberString);
+        }
+        ArticleListModel articleListModel = listSerivce.getArticleList(pageNumber);
+        model.addAttribute("listModel", articleListModel);
+
+        if (articleListModel.getTotalPageCount() > 0) {
+            int beginPageNumber =
+                    (articleListModel.getRequestPage() - 1) / 10 * 10 + 1;
+            int endPageNumber = beginPageNumber + 9;
+            if (endPageNumber > articleListModel.getTotalPageCount()) {
+                endPageNumber = articleListModel.getTotalPageCount();
+            }
+            model.addAttribute("beginPage", beginPageNumber);
+            model.addAttribute("endPage", endPageNumber);
+        }
+        return "list_view";
+    }
+
+    @GetMapping("/read")
+    public String read(Model model, HttpServletRequest request) {
+        int articleId = Integer.parseInt(request.getParameter("articleId"));
+        String viewPage = null;
+        try {
+            model.addAttribute("article", readSerivce.readArticle(articleId));
+
+            viewPage = "/read_view.jsp";
+        } catch (ArticleNotFoundException e) {
+            viewPage = "/article_not_found.jsp";
+        }
+        return "read_view";
+    }
+
+    @GetMapping("/writeForm")
+    public String writeForm(Model model) {
+        return "writeForm";
+    }
+
+
+}
