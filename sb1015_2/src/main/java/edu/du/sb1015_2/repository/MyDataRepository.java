a0b83676841bf49bac3ad8b018756acf20c590eb
ChangHyun <87058844+whatisyourcode@users.noreply.github.com>
2024-10-21 13:05:54 +0900

    Delete sb1015_2 directory

--- sb1015_2/src/main/java/edu/du/sb1015_2/repository/MyDataRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.du.sb1015_2.repository;
-
-import edu.du.sb1015_2.entity.MyData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyDataRepository extends JpaRepository<MyData, Long> {
-}
